a8335b4e7b38f419bbefa82627bb7718395ce8d6
git@github.com:Romario0895/lab-natty-or-not.git <rromario120@hotmail.com>
2024-07-14 16:56:51 -0300

    inicial commit

---
+++ src/main/java/me/dio/Stander_dev_me_week_java/Application.java
@@ -0,0 +1,13 @@
+package me.dio.Stander_dev_me_week_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/me/dio/Stander_dev_me_week_java/StanderDevMeWeekJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.Stander_dev_me_week_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StanderDevMeWeekJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
